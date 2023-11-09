7de681192790002b05253bbf731c84e60dea13e2
shion0224 <shion8214@gmail.com>
2023-11-09 20:37:27 +0900

    TypeScript勉強会

---
+++ public/index.ts
@@ -0,0 +1,11 @@
+/// <reference lib="dom"/>
+import { sum } from "./sum.ts";
+
+const message = await fetch("/welcome-message");
+const element = document.querySelector("body");
+if (element !== null) {
+    element.innerHTML =
+    `<h1>${await message.text()}</h1>` + `<p>${sum(123, 456)}</p>`;
+}else{
+    console.log("element is null");
+}
